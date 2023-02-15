d1d82c4ec4da68a32f56ba626fbc2e4ff436ca9a
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2023-02-14 22:35:55 -0500

    + operation expression

--- src/Expression.cs
+++ src/Expression.cs
@@ -2,6 +2,11 @@ namespace PMath
 {
     public class Expression
     {
-        
+        List<Operation> Operations = new List<Operation>();
+
+        public Expression(List<Operation> operations)
+        {
+            Operations = operations;
+        }
     }
 }
