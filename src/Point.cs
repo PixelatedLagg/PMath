ee61c1a2280e3efc3dc173615417939fb89fa4f4
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2023-02-24 22:23:50 -0500

    + distance between points

--- src/Point.cs
+++ src/Point.cs
@@ -12,7 +12,7 @@ namespace PMath
 
         public static double DistanceBetween(Point p1, Point p2)
         {
-            return p1;
+            return Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2));
         }
     }
 }
