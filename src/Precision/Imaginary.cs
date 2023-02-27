090cce94a2ce883f7f1d150354275601fadb9e6b
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2023-02-26 22:29:55 -0500

    + imaginary related

--- src/Precision/Imaginary.cs
+++ src/Precision/Imaginary.cs
@@ -2,6 +2,11 @@ namespace PMath
 {
     public struct Imaginary //equal to sqrt(-1) * x
     {
+        public double Value;
 
+        public Imaginary(double value)
+        {
+            Value = value;
+        }
     }
 }
