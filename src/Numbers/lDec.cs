d379f8a72a860e12e44bb3fea442a5bf764dd080
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2023-09-22 22:48:34 -0400

    + more methods for ldec

--- src/Numbers/lDec.cs
+++ src/Numbers/lDec.cs
@@ -28,5 +28,32 @@ namespace PMath
         {
             //divide (return possible irrational)
         }
+
+
+
+        public static ldec operator +(int a, ldec b)
+        {
+            //add
+        }
+
+        public static ldec operator -(int a, ldec b)
+        {
+            //subtract
+        }
+
+        public static ldec operator *(int a, ldec b)
+        {
+            //multiply
+        }
+
+        public static pirr operator /(int a, ldec b)
+        {
+            //divide (return possible irrational)
+        }
+
+        public override string ToString()
+        {
+            //return string
+        }
     }
 }
