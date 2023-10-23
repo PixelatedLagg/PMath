4eea3e12ae61da3075ec4e0bc816fa25b84e7b7a
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2023-10-22 23:31:40 -0400

    q3 not working :/

--- Program.cs
+++ Program.cs
@@ -5,7 +5,12 @@ class Program
 {
     public static void Main()
     {
-        QSet32 set = new("test", 1, 2, 3, 4, 5, 6, 7, 8);
+        QSet32 set = new("test", -8035, -7798, -7008, -5525, 4863, 3218, -3000, 5990, 7175, -1044);
         Console.WriteLine(set.QuickSummary());
+        Console.Write("Outliers:");
+        foreach (int i in set.Outliers())
+        {
+            Console.Write($" {i}");
+        }
     }
 }
