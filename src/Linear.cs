f927ce5cd20934b37217838e9ab3b5d25345ed6b
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2023-11-01 23:45:13 -0400

    finished linear inverse()

--- src/Linear.cs
+++ src/Linear.cs
@@ -10,10 +10,7 @@ namespace PMath
             B = b;
         }
 
-        public Linear Inverse()
-        {
-            //inverse code
-        }
+        public Linear Inverse() => new(1 / M, -1 * B / M);
 
         public double Input(double x) => x * M + B;
         public double Input(int x) => x * M + B;
