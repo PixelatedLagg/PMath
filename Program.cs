c309a0be5bf3ef6ab797aed647d2cf20a8446e8a
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2024-02-06 23:33:29 -0500

    reverse factorial

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
-﻿using ExtendedNumerics;
+﻿using System.Numerics;
+using ExtendedNumerics;
 using PMath;
 using PMath.Statistics;
-
+using System.Globalization;
 class Program
 {
     public static void Main()
     {
-        var test = Trig.Sin(0.75);
+        BigInteger b = BigInteger.Parse("5.50262215981e+57", NumberStyles.AllowDecimalPoint | NumberStyles.AllowExponent); //45!
+        Console.WriteLine(b.ReverseFactorial());
     }
 }
--- src/NumberExtensions.cs
+++ src/NumberExtensions.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 namespace PMath
 {
     public static class NumberExtensions
@@ -14,6 +16,42 @@ namespace PMath
                 res *= i;
             }
             return res;
+        }
+
+        public static BigInteger Factorial(this BigInteger x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            int res = 1;
+            for (int i = 2; i <= x; i++)
+            {
+                res *= i;
+            }
+            return res;
+        }
+
+        public static int ReverseFactorial(this int x)
+        {
+            int i = 1, result = x;
+            while((result / (i + 1)) >= 1)
+            {
+                result /= i;
+                i++;
+            }
+            return result;
+        }
+
+        public static BigInteger ReverseFactorial(this BigInteger x)
+        {
+            BigInteger i = 1, result = x;
+            while((result / (i + 1)) >= 1)
+            {
+                result /= i;
+                i++;
+            }
+            return result;
         }
     }
 }
